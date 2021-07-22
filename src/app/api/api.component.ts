0a86e60d657f314553d7f4660ef5d119c6908b40
malikromir <malikromir@gmail.com>
2021-07-22 10:35:46 +0530

    routing(3)

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,15 +1,19 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import SwaggerUI from 'swagger-ui';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-api',
   templateUrl: './api.component.html',
   styleUrls: ['./api.component.css']
 })
-export class ApiComponent implements AfterViewInit {
-  @Input() spec: any;
-  constructor() {}
+export class ApiComponent implements AfterViewInit, OnInit {
+  spec: any;
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit() {
+    this.spec = this.apiService.getApi;
+  }
   ngAfterViewInit() {
     const ui = SwaggerUI({
       domNode: document.getElementById('swagger-ui'),
