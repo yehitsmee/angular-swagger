3e1a5bba2ae98e3214e9d6f269acd7a6c0093de2
malikromir <malikromir@gmail.com>
2021-07-22 20:14:25 +0530

    toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { ApiComponent } from './api/api.component';
@@ -13,10 +18,15 @@ import { ApiService } from './api.service';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     MatCardModule,
     MatButtonModule,
-    RoutingModule
+    RoutingModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   declarations: [AppComponent, ApiComponent, CardComponent],
   providers: [ApiService],
